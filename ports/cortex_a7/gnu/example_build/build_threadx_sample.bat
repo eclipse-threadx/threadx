9489d3bea8fea18d59ca391d140934fd964be2fc
Yajun Xia <yajunxia@microsoft.com>
2023-09-13 15:27:40 +0800

    update for Cortex-A9.

--- ports/cortex_a7/gnu/example_build/build_threadx_sample.bat
+++ ports/cortex_a7/gnu/example_build/build_threadx_sample.bat
@@ -2,9 +2,7 @@ arm-none-eabi-gcc -c -g -mcpu=cortex-a7 reset.S
 arm-none-eabi-gcc -c -g -mcpu=cortex-a7 crt0.S
 arm-none-eabi-gcc -c -g -mcpu=cortex-a7 tx_initialize_low_level.S
 arm-none-eabi-gcc -c -g -mcpu=cortex-a7 MP_GIC.s
+arm-none-eabi-gcc -c -g -mcpu=cortex-a7 MP_PrivateTimer.s
 arm-none-eabi-gcc -c -g -mcpu=cortex-a7 V7.s
 arm-none-eabi-gcc -c -g -mcpu=cortex-a7 -I../../../../common/inc -I../inc sample_threadx.c
-arm-none-eabi-gcc -g -nostartfiles -mcpu=cortex-a7 -T sample_threadx.ld --specs=nosys.specs -o sample_threadx.out -Wl,-Map=sample_threadx.map MP_GIC.o V7.o crt0.o reset.o tx_initialize_low_level.o sample_threadx.o tx.a
-
-
-
+arm-none-eabi-gcc -g -nostartfiles -mcpu=cortex-a7 -T sample_threadx.ld --specs=nosys.specs -o sample_threadx.out -Wl,-Map=sample_threadx.map MP_GIC.o MP_PrivateTimer.o V7.o crt0.o reset.o tx_initialize_low_level.o sample_threadx.o tx.a
--- ports/cortex_a8/gnu/example_build/build_threadx_sample.bat
+++ ports/cortex_a8/gnu/example_build/build_threadx_sample.bat
@@ -2,7 +2,7 @@ arm-none-eabi-gcc -c -g -mcpu=cortex-a8 reset.S
 arm-none-eabi-gcc -c -g -mcpu=cortex-a8 crt0.S
 arm-none-eabi-gcc -c -g -mcpu=cortex-a8 tx_initialize_low_level.S
 arm-none-eabi-gcc -c -g -mcpu=cortex-a8 MP_GIC.s
+arm-none-eabi-gcc -c -g -mcpu=cortex-a8 MP_PrivateTimer.s
 arm-none-eabi-gcc -c -g -mcpu=cortex-a8 V7.s
 arm-none-eabi-gcc -c -g -mcpu=cortex-a8 -I../../../../common/inc -I../inc sample_threadx.c
-arm-none-eabi-gcc -g -nostartfiles -mcpu=cortex-a8 -T sample_threadx.ld --specs=nosys.specs -o sample_threadx.out -Wl,-Map=sample_threadx.map MP_GIC.o V7.o crt0.o reset.o tx_initialize_low_level.o sample_threadx.o tx.a
-
+arm-none-eabi-gcc -g -nostartfiles -mcpu=cortex-a8 -T sample_threadx.ld --specs=nosys.specs -o sample_threadx.out -Wl,-Map=sample_threadx.map MP_GIC.o MP_PrivateTimer.o V7.o crt0.o reset.o tx_initialize_low_level.o sample_threadx.o tx.a
