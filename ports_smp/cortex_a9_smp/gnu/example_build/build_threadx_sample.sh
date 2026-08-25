f82a349b8d6f663d4722c59df3892fbdbf5ac68c
Prashit Vora <prashitvora2006@gmail.com>
2026-08-25 23:22:29 +0530

    ports: add Linux build scripts for Cortex-A7 and A9 SMP

---
+++ ports_smp/cortex_a9_smp/gnu/example_build/build_threadx_sample.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+set -e
+arm-none-eabi-gcc -c -g -I../../../../common_smp/inc -I../inc -mcpu=cortex-a9 sample_threadx.c
+arm-none-eabi-gcc -c -g -mcpu=cortex-a9 startup.S
+arm-none-eabi-gcc -c -g -mcpu=cortex-a9 MP_GIC.S
+arm-none-eabi-gcc -c -g -mcpu=cortex-a9 MP_SCU.S
+arm-none-eabi-gcc -c -g -mcpu=cortex-a9 MP_Mutexes.S
+arm-none-eabi-gcc -c -g -mcpu=cortex-a9 MP_PrivateTimer.S
+arm-none-eabi-gcc -c -g -mcpu=cortex-a9 v7.S
+arm-none-eabi-gcc -T sample_threadx.ld -e Vectors -mcpu=cortex-a9 --specs=nosys.specs -o sample_threadx.out MP_PrivateTimer.o MP_GIC.o MP_Mutexes.o MP_SCU.o sample_threadx.o startup.o v7.o tx.a -Wl,-M > sample_threadx.map
