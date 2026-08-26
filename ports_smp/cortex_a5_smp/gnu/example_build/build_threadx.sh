5521a87e81c0f90f95b0c78dbde6b6eb739b1dca
Prashit Vora <prashitvora2006@gmail.com>
2026-08-26 23:59:37 +0530

    ports: add license headers to Armv7 SMP scripts

--- ports_smp/cortex_a5_smp/gnu/example_build/build_threadx.sh
+++ ports_smp/cortex_a5_smp/gnu/example_build/build_threadx.sh
@@ -1,4 +1,14 @@
 #!/bin/sh
+##############################################################################
+# Copyright (c) 2026 Eclipse ThreadX contributors
+#
+# This program and the accompanying materials are made available under the
+# terms of the MIT License which is available at
+# https://opensource.org/licenses/MIT.
+#
+# SPDX-License-Identifier: MIT
+##############################################################################
+
 set -e
 cd "$(dirname "$0")"
 
